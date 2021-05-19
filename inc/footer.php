4ae0b9725308e70f9a575b0987f9386c11c46131
codepreacher <75647192+code-preacher@users.noreply.github.com>
2021-05-19 08:01:21 -0700

    Delete inc directory

--- inc/footer.php
+++
@@ -1,8 +0,0 @@
- <!-- footer -->
-            <footer class="footer"> 
-                         &copy;  <span>HOMICIDE CASES REPORTING SYSTEM</span> 
-                         <script> 
-                            var r=new Date();
-                            document.write(r.getFullYear());
-                     </script> &nbsp; - All Rights Reserved.
-                    </footer>
